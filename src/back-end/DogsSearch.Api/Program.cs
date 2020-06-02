ff305a639114a051f5d011183d3b226d628b7525
mykhailo.romaniuk <mishani0x0ef@gmail.com>
2020-06-02 11:15:05 +0300

    Integrate Sentry into .NET

--- src/back-end/DogsSearch.Api/Program.cs
+++ src/back-end/DogsSearch.Api/Program.cs
@@ -21,6 +21,7 @@ namespace DogsSearch.Api
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
-                .UseNLog();
+                // .UseNLog()
+                .UseSentry();
     }
 }
