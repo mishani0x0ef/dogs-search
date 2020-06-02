2d1bf3621529d1b8b5f6da15ff078e5947a5ea2f
mykhailo.romaniuk <mishani0x0ef@gmail.com>
2020-06-02 11:04:42 +0300

    Add db logging

--- src/database/create-logs.sql
+++ src/database/create-logs.sql
@@ -3,7 +3,7 @@ go
 
 create table Log
 (
-    DogId bigint identity(1, 1) primary key,
+    LogId bigint identity(1, 1) primary key,
     Level nvarchar(10) not null,
     Message nvarchar(max),
     LoggedAt datetime default getdate()
