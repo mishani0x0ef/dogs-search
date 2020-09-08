0afd5478d90e620e32f5031c9e70cdb92202dc7f
redbastard <andrewkrilik@gmail.com>
2020-09-08 19:13:25 +0300

    card grid fixed w/ grid css, card btns correctly placed

--- src/front-end/src/app/adoption/adoption-page/adoption-page.component.ts
+++ src/front-end/src/app/adoption/adoption-page/adoption-page.component.ts
@@ -7,7 +7,7 @@ import { Title } from '@angular/platform-browser';
 @Component({
   selector: 'app-adoption-page',
   templateUrl: './adoption-page.component.html',
-  styleUrls: ['./adoption-page.component.scss']
+  styleUrls: ['./adoption-page.component.scss'],
 })
 export class AdoptionPageComponent extends PageComponent implements OnInit {
   dogs: Dog[] = [];
@@ -23,14 +23,15 @@ export class AdoptionPageComponent extends PageComponent implements OnInit {
       name: 'Кекс',
       age: 4,
       gender: 'male',
-      imageUrl: 'https://instagram.flwo1-1.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/s640x640/83996290_102462877949490_6913145271293721410_n.jpg?_nc_ht=instagram.flwo1-1.fna.fbcdn.net&_nc_cat=106&_nc_ohc=ytyVhp5qGKMAX_BcdsL&oh=3286bbe231b4344b537253f955f959d3&oe=5EBE1E1B'
+      imageUrl: 'https://i.ytimg.com/vi/1Ne1hqOXKKI/maxresdefault.jpg',
     };
     const female: Dog = {
       id: '1232fafa-14124-gsaga-12e21w',
       name: 'Cookie',
       age: 1.5,
       gender: 'female',
-      imageUrl: 'https://scontent.flwo1-1.fna.fbcdn.net/v/t1.0-9/89291781_3054515121277323_7743920107356684288_n.jpg?_nc_cat=105&_nc_sid=110474&_nc_ohc=Kv_AWVpjVSsAX9LiWJw&_nc_ht=scontent.flwo1-1.fna&oh=a869745290ee39a44a362231c6bcaffa&oe=5EBA6424'
+      imageUrl:
+        'https://ichef.bbci.co.uk/news/976/cpsprodpb/12A9B/production/_111434467_gettyimages-1143489763.jpg',
     };
     this.dogs = [male, male, female, male, female, female];
   }
--- src/front-end/src/app/shared/components/dog-card/dog-card.component.ts
+++ src/front-end/src/app/shared/components/dog-card/dog-card.component.ts
@@ -5,11 +5,12 @@ import { Dog } from '../../models';
 @Component({
   selector: 'app-dog-card',
   templateUrl: './dog-card.component.html',
-  styleUrls: ['./dog-card.component.scss']
+  styleUrls: ['./dog-card.component.scss'],
 })
 export class DogCardComponent {
   @Input()
   dog: Dog;
+  flipped: boolean;
 
   get imageUrl(): string {
     return `url(${this.dog.imageUrl})`;
@@ -17,9 +18,20 @@ export class DogCardComponent {
 
   get info() {
     return this.dog.gender === 'female'
-      ? { status: 'danger', adoptionText: 'Удочерити', textStatus: 'text-danger' }
-      : { status: 'primary', adoptionText: 'Усиновити', textStatus: 'text-primary' };
+      ? {
+          status: 'danger',
+          adoptionText: 'Удочерити',
+          textStatus: 'text-danger',
+        }
+      : {
+          status: 'primary',
+          adoptionText: 'Усиновити',
+          textStatus: 'text-primary',
+        };
+  }
+  toggleView() {
+    this.flipped = !this.flipped;
   }
 
-  constructor() { }
+  constructor() {}
 }
